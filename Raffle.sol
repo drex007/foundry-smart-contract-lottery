3a75289576b634c86592487d8b0b6c298696cc07
drex007 <vykeoj@gmail.com>
2024-12-16 00:46:45 +0100

    feat: added enter rafflle and getter funcs

--- Raffle.sol
+++ Raffle.sol
@@ -2,4 +2,41 @@
 
 pragma solidity  0.8.24;
 
-contract Raffle {}
+
+/**
+ * @title  A raffle contract
+ * @author Viktor Ojemeni
+ * @notice Just for creating a simple raffle 
+ * @dev Implement chainVRF
+ */
+
+contract Raffle {
+
+    /** Errors */
+    error sendMoreToEnterRaffle();
+
+    uint256 private immutable i_entranceFee; //immutable variable
+
+
+    constructor(uint256 entranceFee){
+        i_entranceFee = entranceFee;
+
+    }
+
+    function enterRaffle() public payable{
+        if(msg.value < i_entranceFee){
+            revert sendMoreToEnterRaffle() ;
+        }
+
+    }
+
+    function pickWinner() public {
+
+    }
+    /** Getter functions */
+
+    function getEntranceFee() external view returns(uint256) {
+        return i_entranceFee;
+        
+     }
+}
